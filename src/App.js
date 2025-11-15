44f902305ac443d82608814a19ba739028eb3513
Phumlani Mbabela <pmbabela@gmail.com>
2025-11-15 15:00:29 +0200

    Updated code. I'm busy linking the backend.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
+// src/App.js
 import React from "react";
 import Index from "./pages/Index";
 
-//Css
+// React Query
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+// Css
 import "./assets/vendor/switcher/switcher.css";
 import "./assets/vendor/swiper/swiper-bundle.min.css";
-
 import "./assets/css/style.css";
 
+// Create a single QueryClient instance for the whole app
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Index />
-    </>
+    </QueryClientProvider>
   );
 }
 
 export default App;
-
-
