dce770ae784e59d1471c34a17c875360ed14f5d8
Phumlani Mbabela <pmbabela@gmail.com>
2025-11-15 14:55:04 +0200

    The folder src/src has been removed.

--- src/src/store/auth.js
+++
@@ -1,9 +0,0 @@
-import { create } from "zustand";
-export const useAuth = create((set, get) => ({
-  user: null,
-  subscription: null,
-  setUser: (user) => set({ user }),
-  setSubscription: (subscription) => set({ subscription }),
-  isPremium: () => get().subscription?.status === "active",
-}));
-
--- src/src/store/tenant.js
+++
@@ -1,3 +0,0 @@
-import { create } from "zustand";
-export const useTenant = create((set) => ({ tenantId: null, setTenantId: (tenantId) => set({ tenantId }) }));
-
